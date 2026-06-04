518257392798ec1cdae324bd23c18236e968197e
shees95 <shees9565@gmail.com>
2026-06-04 11:12:47 +0900

    Init

---
+++ Source/CH3_Personal/CH3_Personal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CH3_Personal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CH3_Personal, "CH3_Personal" );
---
+++ Source/CH3_Personal/MyBase.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class CH3_PERSONAL_API MyBase
+{
+public:
+	MyBase();
+	~MyBase();
+};
