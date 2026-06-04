c4bcc54452302042c06c30ff7bf1719896f487cc
shees95 <shees9565@gmail.com>
2026-06-04 12:52:44 +0900

    Init

---
+++ Source/CH3_Personal_1/CH3_Personal_1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CH3_Personal_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CH3_Personal_1, "CH3_Personal_1" );
