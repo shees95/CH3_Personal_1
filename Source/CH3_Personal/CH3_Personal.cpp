8309f0cc13f4984840ac0c156e8580d520611e03
shees95 <shees9565@gmail.com>
2026-06-04 12:05:18 +0900

    refac: version up from 5.4 to 5.5

--- Source/CH3_Personal/CH3_Personal.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CH3_Personal.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CH3_Personal, "CH3_Personal" );
